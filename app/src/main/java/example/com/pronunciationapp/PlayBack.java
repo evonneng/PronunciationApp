51a444c6262a1fd573c22fc5c95a9709c99d48cc
Evonne Ng <evonne.ng@clover.com>
2016-06-21 22:47:28 -0700

    added playback capabilities: sound not working

--- app/src/main/java/example/com/pronunciationapp/PlayBack.java
+++ app/src/main/java/example/com/pronunciationapp/PlayBack.java
@@ -6,43 +6,83 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
 
 import java.util.Locale;
 
-public class PlayBack extends AppCompatActivity {
-    TextToSpeech player;
+public class PlayBack extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener{
+    //TTS object
+    private TextToSpeech myTTS;
+    //status check code
+    private int MY_DATA_CHECK_CODE = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    //create the Activity
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_back);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        //show the text typed in by the user
+        //get a reference to the button element listed in the XML layout
+        Button speakButton = (Button)findViewById(R.id.play_button);
+        //listen for clicks
+        speakButton.setOnClickListener(this);
+
+        //check for TTS data
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+    }
+
+    //respond to button clicks
+    public void onClick(View v) {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView usrTxt = new TextView(this);
-        usrTxt.setText(message);
         usrTxt.setTextSize(40);
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.usrTxt);
+        usrTxt.setText(message);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.playback);
         layout.addView(usrTxt);
-
-        //initializing the player
-        player=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
-                    player.setLanguage(Locale.UK);
-                }
-            }
-        });
-        //play the string message passed to the player
-        player.speak(message, TextToSpeech.QUEUE_FLUSH, null);
     }
 
+    //speak the user text
+    private void speakWords(String speech) {
+        //speak straight away
+        
+    }
+
+    //act on result of TTS data check
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                //the user has the necessary data - create the TTS
+                myTTS = new TextToSpeech(this, this);
+            }
+            else {
+                //no data - install it now
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
+    }
+
+    //setup TTS
+    public void onInit(int initStatus) {
+
+        //check for successful instantiation
+        if (initStatus == TextToSpeech.SUCCESS) {
+            if(myTTS.isLanguageAvailable(Locale.US)==TextToSpeech.LANG_AVAILABLE)
+                myTTS.setLanguage(Locale.US);
+        }
+        else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Unable to read input...", Toast.LENGTH_LONG).show();
+        }
+    }
 }
