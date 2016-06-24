95d99743e3e862befa5d6f438d0fecacd3f1402f
Evonne Ng <evonne.ng@clover.com>
2016-06-24 15:04:59 -0700

    lil bit o' the UI/UX re-designing

--- app/src/main/java/example/com/pronunciationapp/MainActivity.java
+++ app/src/main/java/example/com/pronunciationapp/MainActivity.java
@@ -1,25 +1,19 @@
 package example.com.pronunciationapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     public final static String EXTRA_MESSAGE = "example.com.pronunciationapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
     @Override
--- app/src/main/java/example/com/pronunciationapp/PlayBack.java
+++ app/src/main/java/example/com/pronunciationapp/PlayBack.java
@@ -1,12 +1,8 @@
 package example.com.pronunciationapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -16,7 +12,7 @@ import android.widget.Toast;
 
 import java.util.Locale;
 
-public class PlayBack extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener{
+public class PlayBack extends Activity implements View.OnClickListener, TextToSpeech.OnInitListener{
     //TTS object
     private TextToSpeech myTTS;
     //status check code
